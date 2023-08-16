20ff7aa638dbf9cee1f1ffb6c6a35733441f9d04
Axel <axeltoure1eme@gmail.com>
2023-08-16 07:55:13 +0000

    Creation de la page de inscription

---
+++ resources/views/auth/signup.blade.php
@@ -0,0 +1,136 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Inscription</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+<body class="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
+    <section class="gradient-form h-full bg-neutral-200 dark:bg-neutral-700">
+        <div class="container h-full p-10">
+            <div class="g-6 flex h-full flex-wrap items-center justify-center text-neutral-800 dark:text-neutral-200">
+                <div class="w-full">
+                    <div class="block rounded-lg bg-white shadow-lg dark:bg-neutral-800">
+                        <div class="g-0 lg:flex lg:flex-wrap">
+                            <!-- left column container with background and description-->
+                            <div class="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none" style="background: linear-gradient(to right, #006A4E, #006A4E, #006A4E, #006A4E)">
+                                <div class="px-4 py-6 text-white md:mx-6 md:p-12">
+                                    <h4 class="mb-6 text-xl font-semibold">
+                                        Nous sommes plus qu'une simple entreprise
+                                    </h4>
+                                    <p class="text-sm">
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                                        elit, sed do eiusmod tempor incididunt ut labore et
+                                        dolore magna aliqua. Ut enim ad minim veniam, quis
+                                        nostrud exercitation ullamco laboris nisi ut aliquip ex
+                                        ea commodo consequat.
+                                    </p>
+                                </div>
+                            </div>
+                            <!-- Left column container-->
+                            <div class="px-4 md:px-0 lg:w-6/12">
+                                <div class="md:mx-6 md:p-12">
+                                    <!--Logo-->
+                                    <div class="text-center">
+                                        <img class="mx-auto w-48" src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp" alt="logo" />
+                                        <h4 class="mb-5 mt-1 pb-1 text-xl font-semibold">
+                                            Nous sommes l'équipe K-SIRA
+                                        </h4>
+                                    </div>
+                                    <div class="text-center">
+                                        @error('password')
+                                        <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Exigences relatives au mot de passe :</h2>
+
+                                        <h2 class="mb-2 text-lg font-semibold text-gray-900 dark:text-white">Password requirements:</h2>
+                                        <ul class="max-w-md space-y-1 text-gray-500 list-inside dark:text-gray-400">
+                                            <li class="flex items-center">
+                                                <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                                </svg>
+                                                At least 10 characters
+                                            </li>
+                                            <li class="flex items-center">
+                                                <svg class="w-3.5 h-3.5 mr-2 text-green-500 dark:text-green-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                                </svg>
+                                                At least one lowercase character
+                                            </li>
+                                            <li class="flex items-center">
+                                                <svg class="w-3.5 h-3.5 mr-2 text-gray-500 dark:text-gray-400 flex-shrink-0" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" />
+                                                </svg>
+                                                At least one special character, e.g., ! @ # ?
+                                            </li>
+                                        </ul>
+
+
+                                        @endif
+
+                                        @if ($errors->has('message'))
+                                        <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+                                            <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+                                            </svg>
+                                            <span class="sr-only">Info</span>
+                                            <div>
+                                                <span class="font-medium">Danger alert !</span> {{ $errors->first('message') }}.
+                                            </div>
+                                        </div>
+                                        @endif
+                                    </div>
+
+                                    <form method='POST' action="{{ route('signup') }}">
+                                        <p class="mb-4">Veuillez reseinger votre information pour votre votre compte</p>
+                                        <!--Username input-->
+                                        @csrf
+                                        <div class="relative mb-4" data-te-input-wrapper-init>
+                                            <input type="text" name="username" id="floating_filled" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                            <label for="floating_filled" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Nom d'utilisateur</label>
+                                            @error('username')
+                                            <div class="error">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <!--Password input-->
+                                        <div class="relative mb-4" data-te-input-wrapper-init>
+                                            <input name='password' type="text" id="floating_filled" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                            <label for="floating_filled" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Mot de passe </label>
+                                            @error('password')
+                                            <div class="error">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <!--Password Confirmed input-->
+                                        <div class="relative mb-4" data-te-input-wrapper-init>
+                                            <input name="password_confirmation" type="text" id="floating_filled" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                            <label for="floating_filled" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Confirmation le mot de passe </label>
+                                            @error('password_confirmation')
+                                            <div class="error">{{ $message }}</div>
+                                            @enderror
+                                        </div <!--Submit button-->
+                                        <div class="mb-12 pb-1 pt-1 text-center">
+                                            <button type="submit" class="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]" type="button" data-te-ripple-init data-te-ripple-color="light" style="
+                              background: linear-gradient(to right, #006A4E, #006A4E, #006A4E, #006A4E);
+                            ">
+                                                S'INSCRIPTION </button>
+
+                                            <!--Forgot password link-->
+                                            <a href="#!">Mot de passe oublié ?</a>
+                                        </div>
+
+                                        <!--Register button-->
+                                        <div class="flex items-center justify-between pb-6">
+                                            <a href='{{ route('LoginForm') }}' class="mb-0 mr-2">Connexion </a>
+
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
+
