148f248fec82ea5aaff5de9e134ccca4c905895b
Axel <axeltoure1eme@gmail.com>
2023-08-17 12:26:50 +0000

    Ajout des lien sur les autre page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -57,7 +57,7 @@
                     {{ $user->username }}
                 @endisset
               </h2>
-              <p class="text-xs text-gray-500 text-center">Administrator</p>
+              <p class="text-xs text-gray-500 text-center">Administrateur</p>
             </div>
           </div>
           <div
@@ -87,7 +87,7 @@
           </div>
           <div id="menu" class="flex flex-col space-y-2">
             <a
-              href=""
+              href="{{ route('Dashboard') }}"
               class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:text-base rounded-md transition duration-150 ease-in-out"
             >
               <svg
@@ -103,7 +103,7 @@
               <span class=""> Tableau de bord</span>
             </a>
             <a
-              href=""
+              href="{{ route('Liste') }}"
               class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
             >
               <svg
@@ -116,10 +116,10 @@
                   d="M11 17a1 1 0 001.447.894l4-2A1 1 0 0017 15V9.236a1 1 0 00-1.447-.894l-4 2a1 1 0 00-.553.894V17zM15.211 6.276a1 1 0 000-1.788l-4.764-2.382a1 1 0 00-.894 0L4.789 4.488a1 1 0 000 1.788l4.764 2.382a1 1 0 00.894 0l4.764-2.382zM4.447 8.342A1 1 0 003 9.236V15a1 1 0 00.553.894l4 2A1 1 0 009 17v-5.764a1 1 0 00-.553-.894l-4-2z"
                 ></path>
               </svg>
-              <span class="">Products</span>
+              <span class="">Liste des Professionnels </span>
             </a>
             <a
-              href=""
+              href="{{ route('Fiche') }}"
               class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
             >
               <svg
@@ -135,10 +135,10 @@
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <span class="">Reports</span>
+              <span class="">Fiche de renseignement</span>
             </a>
             <a
-              href=""
+              href="{{ route('Messages') }}"
               class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
             >
               <svg
@@ -172,7 +172,7 @@
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <span class="">Calendar</span>
+              <span class=""> Calendrier</span>
             </a>
             <a
               href=""
@@ -190,23 +190,7 @@
                   clip-rule="evenodd"
                 ></path>
               </svg>
-              <span class="">Table</span>
-            </a>
-            <a
-              href=""
-              class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
-            >
-              <svg
-                class="w-6 h-6 fill-current inline-block"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z"
-                ></path>
-              </svg>
-              <span class="">UI Components</span>
+              <span class=""> Tableau</span>
             </a>
             <a
               href=""
@@ -222,8 +206,25 @@
                   d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z"
                 ></path>
               </svg>
-              <span class="">Users</span>
+              <span class="">Utilisateurs</span>
             </a>
+            <a
+              href="{{ route('logout') }}"
+              class="text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white hover:scale-105 rounded-md transition duration-150 ease-in-out"
+            >
+              <svg
+                class="w-6 h-6 fill-current inline-block"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM14 11a1 1 0 011 1v1h1a1 1 0 110 2h-1v1a1 1 0 11-2 0v-1h-1a1 1 0 110-2h1v-1a1 1 0 011-1z"
+                ></path>
+              </svg>
+              <span class="">Deconnexion</span>
+            </a>
+
           </div>
         </div>
       </div>
