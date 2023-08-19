558c3a7ace3641b9cb9f4a82999d262542da7dda
Axel <axeltoure1eme@gmail.com>
2023-08-19 23:35:22 +0000

    Defintion des donnés a enregistrer

--- app/Models/habitation.php
+++ app/Models/habitation.php
@@ -5,7 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class habitation extends Model
+class Habitation extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'Ville',
+        'Commune',
+        'Quartier',
+        'SituationMatrimoliale',
+        'RégimeMatrimoliale',
+        'id_Assurance',
+        'id_finance',
+        'organisation_id',
+        'charge_id',
+    ];
 }
