bbf0e94353dd73f02fa563e1a83a37d363362063
Axel <axeltoure1eme@gmail.com>
2023-08-19 23:34:09 +0000

    Defintion des donnés a enregistrer

--- app/Models/Artisan.php
+++ app/Models/Artisan.php
@@ -8,4 +8,24 @@ use Illuminate\Database\Eloquent\Model;
 class Artisan extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        "Nom",
+        "Prenom",
+        "Dtnaissance",
+        "LieuNaissance",
+        "Profession",
+        "AnExpProf",
+        "Sexe",
+        "AnProf",
+        "registreMetier",
+        "Email",
+        "Contact",
+        "id_parrain",
+        "id_habitation",
+        "id_agent",
+        "id_fiche",
+        'id_agent',
+        'id_activite'
+    ];
 }
