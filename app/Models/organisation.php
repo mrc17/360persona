2cffa9fc7c499abc06c4d12b8c116bc89e78f8f4
Axel <axeltoure1eme@gmail.com>
2023-08-19 23:36:15 +0000

    Defintion des donnés a enregistrer

--- app/Models/organisation.php
+++ app/Models/organisation.php
@@ -5,7 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class organisation extends Model
+class Organisation extends Model
 {
     use HasFactory;
+
+    protected $fillable =[
+        'etat',"Nom"
+    ];
 }
