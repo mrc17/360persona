4a80c8ed1cb5ec64eb4271668cf076eae859ea44
Axel <axeltoure1eme@gmail.com>
2023-08-23 06:15:14 +0000

    Ajout des relation

--- app/Models/Parrain.php
+++ app/Models/Parrain.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Artisan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Parrain extends Model
 {
@@ -12,4 +13,8 @@ class Parrain extends Model
     protected $fillable=[
         'Nom',"Prenom","sexe","Profession","Appreciation",
     ];
+
+    public function artisan(){
+        return $this->belongsTo(Artisan::class,'id_parrain');
+    }
 }
