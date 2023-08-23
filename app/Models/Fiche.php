edf0592f36ed4780f12ccf1513d90f4df55ada93
Axel <axeltoure1eme@gmail.com>
2023-08-23 06:14:35 +0000

    Ajout des relation

--- app/Models/Fiche.php
+++ app/Models/Fiche.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Artisan;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Fiche extends Model
 {
@@ -16,4 +17,8 @@ class Fiche extends Model
         "zone",
         "ordre"
     ];
+
+    public function artisan(){
+        return $this->belongsTo(Artisan::class,'id_fiche');
+    }
 }
