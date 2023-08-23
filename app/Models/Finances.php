68c75d6f7cd2401c866d284a65dc547193994731
Axel <axeltoure1eme@gmail.com>
2023-08-23 06:14:44 +0000

    Ajout des relation

--- app/Models/Finances.php
+++ app/Models/Finances.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Habitation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Finances extends Model
 {
@@ -11,4 +12,8 @@ class Finances extends Model
     protected $fillable=[
         'etat','Nom',
     ];
+
+    public function habitation(){
+        return $this->belongsTo(Habitation::class,'id_finance');
+    }
 }
