ecf78892c9d1e411331009e218214709a9a17e8a
Axel <axeltoure1eme@gmail.com>
2023-08-23 06:13:29 +0000

    Ajout des relation

--- app/Models/Agent.php
+++ app/Models/Agent.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Habitation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Agent extends Model
 {
@@ -13,4 +14,8 @@ class Agent extends Model
         "Contact",
         "Zone",
     ];
+
+    public function habitation(){
+        return $this->belongsTo(Artisan::class,'id_fiche');
+    }
 }
