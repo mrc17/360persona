2593ab22873e1fca7bd2cfd68a1a5fe66ddb2a0c
Axel <axeltoure1eme@gmail.com>
2023-08-23 06:13:47 +0000

    Ajout des relation

--- app/Models/Assurance.php
+++ app/Models/Assurance.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Habitation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Assurance extends Model
 {
@@ -14,4 +15,8 @@ class Assurance extends Model
         'numero',
         'Agence',
     ];
+
+    public function habitation(){
+        return $this->belongsTo(Habitation::class,'id_Assurance');
+    }
 }
