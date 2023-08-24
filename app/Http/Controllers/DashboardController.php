e7bab2d07ca4fbeeb12cfc882977d91ebcac2eda
Axel <axeltoure1eme@gmail.com>
2023-08-24 19:35:40 +0000

    Compter le nombre d'artisan

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Artisan;
+use App\Models\Parrain;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -10,7 +11,13 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-        return view('Dashboard', ['user' => $user]);
+        // Vous pouvez faire de même pour les autres entités (Agents)
+        $nombreArtisans = Artisan::count();
+        // Vous pouvez faire de même pour les autres entités (Parrains)
+        $nombreParrains = Parrain::count();
+        // Vous pouvez faire de même pour les autres entités (Parrains)
+
+        return view('Dashboard', ['user' => $user, 'nombreArtisans' => $nombreArtisans, "nombreParrains" => $nombreParrains]);
     }
     public function showFiche()
     {
@@ -19,16 +26,21 @@ class DashboardController extends Controller
     }
     public function showListe()
     {
-        $nbrArtisanTotal =Artisan::All()->count();
+        $nbrArtisanTotal = Artisan::All()->count();
         $artisans = Artisan::paginate(12);
         $user = Auth::user();
-        $count=1;
-        return view('Liste', ['user' => $user, 'artisans' => $artisans,"count"=>$count,"nbrArtisanTotal"=>$nbrArtisanTotal]);
+        $count = 1;
+        return view('Liste', ['user' => $user, 'artisans' => $artisans, "count" => $count, "nbrArtisanTotal" => $nbrArtisanTotal]);
     }
 
     public function showMessages()
     {
         $user = Auth::user();
         return view('Fiche', ['user' => $user]);
+    }
+    public function showCalendrier()
+    {
+        $user = Auth::user();
+        return view('Calendrier', ['user' => $user]);
     }
 }
