0febc181487f9e88ef8813ee9f432b168b1832f4
Axel <axeltoure1eme@gmail.com>
2023-08-26 08:22:27 +0000

    Donnée statistique

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Activite;
 use App\Models\Artisan;
 use App\Models\Parrain;
+use App\Models\Activite;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -12,14 +13,69 @@ class DashboardController extends Controller
     public function index()
     {
         $user = Auth::user();
-        // Vous pouvez faire de même pour les autres entités (Agents)
+
+        // Statistiques générales
         $nombreArtisans = Artisan::count();
-        // Vous pouvez faire de même pour les autres entités (Parrains)
         $nombreParrains = Parrain::count();
-        // Vous pouvez faire de même pour les autres entités (Activité)
-        $nombreActivites=Activite::count();
-        return view('Dashboard', ['user' => $user, 'nombreArtisans' => $nombreArtisans, "nombreParrains" => $nombreParrains,"nombreActivites"=>$nombreActivites]);
+        $nombreActivites = Activite::count();
+
+        // Les 4 artisans ayant la plus grande charge
+        $artisansCharge = DB::table('artisans')
+            ->join('charges', 'artisans.id_parrain', '=', 'charges.id')
+            ->orderByDesc('charges.NbrEnfant')
+            ->limit(4)
+            ->get();
+
+        // Le projet le plus coûteux
+        $projetLePlusCouteux = Activite::orderByDesc('CoutestimatifEnchiffre')->first();
+
+        // Répartition des artisans par âge et sexe
+        $artisans = Artisan::all();
+        $hommesJeunes = 0;
+        $hommesMatures = 0;
+        $femmesJeunes = 0;
+        $femmesMatures = 0;
+        $ageSeuil = 30;
+
+        foreach ($artisans as $artisan) {
+            $age = date('Y') - date('Y', strtotime($artisan->Dtnaissance));
+            if ($artisan->Sexe === 'Homme') {
+                if ($age < $ageSeuil) {
+                    $hommesJeunes++;
+                } else {
+                    $hommesMatures++;
+                }
+            } elseif ($artisan->Sexe === 'Femme') {
+                if ($age < $ageSeuil) {
+                    $femmesJeunes++;
+                } else {
+                    $femmesMatures++;
+                }
+            }
+        }
+        $nombreFemmesMaturesMoins30 = Artisan::where('Sexe', 'Femme')
+        ->whereRaw('YEAR(CURDATE()) - YEAR(Dtnaissance) >= 30')
+        ->count();
+
+        $nombreHommesMaturesMoins30 = Artisan::where('Sexe', 'Homme')
+        ->whereRaw('YEAR(CURDATE()) - YEAR(Dtnaissance) >= 30')
+        ->count();
+
+
+        return view('Dashboard', [
+            'user' => $user,
+            'nombreArtisans' => $nombreArtisans,
+            'nombreParrains' => $nombreParrains,
+            'nombreActivites' => $nombreActivites,
+            'artisansCharge' => $artisansCharge,
+            'hommesMatures' => $hommesMatures,
+            'femmesJeunes' => $femmesJeunes,
+            'femmesMatures' => $femmesMatures,
+            'nombreFemmesMaturesMoins30'=>$nombreFemmesMaturesMoins30,
+            'nombreHommesMaturesMoins30'=>$nombreHommesMaturesMoins30
+        ]);
     }
+
     public function showFiche()
     {
         $user = Auth::user();
