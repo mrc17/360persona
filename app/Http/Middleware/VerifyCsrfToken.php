e8557d07c57c57bf67c989713bcf18cb74f99ee5
Axel <axeltoure1eme@gmail.com>
2023-08-30 08:04:02 +0000

    Exclure la verification de token

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'show/artisan/SearchAdvanced', // Nom de votre route
+
     ];
 }
