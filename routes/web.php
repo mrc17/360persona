5d1eba39268d3da1ca3a35879b4a6740395c99ed
Axel Toure <axeltoure1eme@gmail.com>
2023-09-08 17:23:21 +0000

    Updated Laravel Web Routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authentication;
 use App\Http\Controllers\ArtisanController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\GetColonnesController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,12 +24,15 @@ Route::get('/logout', [Authentication::class, 'logout'])->name('logout');
 Route::get('/signup', [Authentication::class, 'showInscription'])->name('LoginSingup');
 
 Route::middleware('auth')->group(function () {
-Route::get('/Dashboard', [DashboardController::class,'index'])->name('Dashboard');
-Route::get('/Fiche', [DashboardController::class,'showFiche'])->name('Fiche');
-Route::get('/Liste', [DashboardController::class,'showListe'])->name('Liste');
-Route::get('/Messages', [DashboardController::class,'showMessages'])->name('Messages');
-Route::get('/Calendrier', [DashboardController::class,'showCalendrier'])->name('Calendrier');
-Route::post('/create/artisan', [ArtisanController::class,'create'])->name('create-artisan');
-Route::get('/show/artisan/{artisan}', [ArtisanController::class, 'show'])->name('show-artisan');
-Route::post('/show/artisan/search', [ArtisanController::class, 'searchartisan'])->name('search-artisan');
+    Route::get('/Dashboard', [DashboardController::class, 'index'])->name('Dashboard');
+    Route::get('/Fiche', [DashboardController::class, 'showFiche'])->name('Fiche');
+    Route::get('/Liste', [DashboardController::class, 'showListe'])->name('Liste');
+    Route::get('/Messages', [DashboardController::class, 'showMessages'])->name('Messages');
+    Route::get('/Calendrier', [DashboardController::class, 'showCalendrier'])->name('Calendrier');
+    Route::post('/create/artisan', [ArtisanController::class, 'create'])->name('create-artisan');
+    Route::get('/show/artisan/{artisan}', [ArtisanController::class, 'show'])->name('show-artisan');
+    Route::post('/show/artisan/search', [ArtisanController::class, 'searchartisan'])->name('search-artisan');
+    Route::get('/showSearchAdvanced', [ArtisanController::class, 'showSearchAdvanced'])->name('showSearchAdvanced');
+    Route::post('/show/artisan/SearchAdvanced', [ArtisanController::class, 'searchartisanavanced'])->name('search-artisan-avanced');
+    Route::get('/getColonnes/{table}', [GetColonnesController::class, 'getColonnes']);
 });
