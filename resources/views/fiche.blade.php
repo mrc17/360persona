dd8c2e542f5a9ecc95907c876863536cf7aaa40c
Axel Toure <axeltoure1eme@gmail.com>
2023-09-09 06:50:05 +0000

    selection  des fonctions des artisans

--- resources/views/fiche.blade.php
+++ resources/views/fiche.blade.php
@@ -176,8 +176,18 @@
                 </div>
                 <div class="grid md:grid-cols-3 mt-5 md:gap-6">
                     <div class="relative z-0 w-full mb-6 group">
-                        <input type="text" name="ProfessionArtisan" value="{{ old('ProfessionArtisan') }}" id="Profession" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                        <label for="Profession" class="peer-focus:font-medium absolute text-sm dark:text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Profession</label>
+                        <select name="ProfessionArtisan" value="{{ old('ProfessionArtisan') }}" id="Profession" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="Profession" />
+                        <option value="">Choisissez une Profession</option>
+                        <option value="Bois">Bois</option>
+                        <option value="Metaux et Mécanique">Metaux et Mécanique</option>
+                        <option value="Textile">Textile</option>
+                        <option value="Batiment">Batiment</option>
+                        <option value="Hygiène et resto">Hygiène et resto</option>
+                        <option value="Electronique">Electronique</option>
+                        <option value="Artisanat d'art">Artisanat d'art</option>
+                        <option value="Autre">Autre</option>
+                        <select
+                        <label for="Profession" class="peer-focus:font-medium absolute text-sm dark:text-white  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"></label>
                         @error('ProfessionArtisan')
                         <p class="text-red-600 italic text-xs">{{ $message }}</p>
                         @enderror
