63332c355ec7d5087efc7fa6e732514e6122074d
Axel Toure <axeltoure1eme@gmail.com>
2023-09-19 23:20:51 +0000

    Creation des colonne aux nouvelle règles [143adf37e5b996fa8dcdd37f125a6b3ff31d30f7]

--- app/Http/Requests/signUpRequest.php
+++ app/Http/Requests/signUpRequest.php
@@ -14,15 +14,46 @@ class SignUpRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'username' => ['required', 'string', 'max:15', 'unique:users,username'],
-            'password' => ['required', 'regex:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/'],
-            'password_confirmation' => ['required', 'same:password'],
+            'Nom' => [
+                'required',
+                'string',
+                'max:15',
+                'unique:agents,nom_agent',
+            ],
+            'conctat' => [
+                'required',
+                'numeric',
+                'digits_between:8,14',
+                'unique:agents,contact_agent',
+            ],
+            'username' => [
+                'required',
+                'string',
+                'max:15',
+                'unique:users,username',
+            ],
+            'password' => [
+                'required',
+                'regex:/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/',
+            ],
+            'password_confirmation' => [
+                'required',
+                'same:password',
+            ],
         ];
     }
 
     public function messages(): array
     {
         return [
+            'Nom.unique' => 'Le nom de l\'agent est déjà utilisé.',
+            'Nom.required' => 'Le nom de l\'agent est obligatoire.',
+            'Nom.string' => 'Le nom de l\'agent doit être une chaîne de caractères.',
+            'Nom.max' => 'Le nom de l\'agent ne doit pas dépasser :max caractères.',
+            'conctat.unique' => 'Le contact de l\'agent est déjà utilisé.',
+            'conctat.required' => 'Le contact de l\'agent est obligatoire.',
+            'conctat.numeric' => 'Le contact de l\'agent doit être numérique.',
+            'conctat.digits_between' => 'Le contact de l\'agent doit avoir entre :min et :max chiffres.',
             'username.unique' => 'Le nom d\'utilisateur est déjà utilisé.',
             'username.required' => 'Le nom d\'utilisateur est obligatoire.',
             'username.string' => 'Le nom d\'utilisateur doit être une chaîne de caractères.',
