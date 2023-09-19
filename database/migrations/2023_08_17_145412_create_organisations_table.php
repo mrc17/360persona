96017362010cc73dacdbffdb7a92c48f7f8017cf
Axel Toure <axeltoure1eme@gmail.com>
2023-09-19 23:32:14 +0000

    correction de tables organistaion

--- database/migrations/2023_08_17_145412_create_organisations_table.php
+++ database/migrations/2023_08_17_145412_create_organisations_table.php
@@ -1,5 +1,6 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,8 +14,8 @@ return new class extends Migration
     {
         Schema::create('organisations', function (Blueprint $table) {
             $table->id();
-            $table->string('etatOrganisation')->nullable();
-            $table->string('NomOrganisation')->nullable();
+            $table->string('etat_organisation')->nullable()->default('non renseigné');
+            $table->string('nom_organisation')->nullable()->default('non renseigné');
             $table->timestamps();
         });
     }
