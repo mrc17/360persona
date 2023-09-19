6bc3ef2ab52644679633d5147b9cf12409c915dd
Axel Toure <axeltoure1eme@gmail.com>
2023-09-19 23:24:01 +0000

    Creation des colonne aux nouvelle règles [cf4b614c6ec1086966e29facea1edca59637041c]

--- app/Models/Artisan.php
+++ app/Models/Artisan.php
@@ -13,37 +13,46 @@ class Artisan extends Model
     use HasFactory;
 
     protected $fillable = [
-        "Nom",
-        "Prenom",
-        "Dtnaissance",
-        "LieuNaissance",
-        "Profession",
-        "AnExpProf",
-        "Sexe",
-        "AnProf",
-        "registreMetier",
-        "Email",
-        "Contact",
+        "nom",
+        "prenom",
+        "dtnaissance",
+        "lieu_naissance",
+        "profession",
+        "an_exp_prof",
+        "sexe",
+        "an_prof",
+        "registre_metier",
+        "email",
+        "contact",
         "id_parrain",
         "id_habitation",
         "id_agent",
         "id_fiche",
-        'id_agent',
         'id_activite'
     ];
 
-    public function habitation(){
-        return $this->belongsTo(Habitation::class,'id_habitation');
+
+
+
+
+
+
+
+    public function habitation()
+    {
+        return $this->belongsTo(Habitation::class, 'id_habitation');
     }
-    public function agent(){
-        return $this->belongsTo(Agent::class,'id_agent');
+    public function agent()
+    {
+        return $this->hasOne(Agent::class, 'id_agent');
     }
 
-    public function parrain(){
-        return $this->belongsTo(Parrain::class,'id_parrain');
+    public function parrain()
+    {
+        return $this->belongsTo(Parrain::class, 'id_parrain');
     }
-    public function fiche(){
-        return $this->belongsTo(Fiche::class,'id_fiche');
+    public function fiche()
+    {
+        return $this->belongsTo(Fiche::class, 'id_fiche');
     }
-
 }
