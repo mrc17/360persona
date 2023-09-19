5b3c01059315fbf479c131b43e7125ca7f8a2195
Axel Toure <axeltoure1eme@gmail.com>
2023-09-19 23:22:43 +0000

    Creation des colonne aux nouvelle règles [143adf37e5b996fa8dcdd37f125a6b3ff31d30f7]

--- app/Models/Activite.php
+++ app/Models/Activite.php
@@ -11,20 +11,21 @@ class Activite extends Model
     use HasFactory;
 
     protected $fillable = [
-        'Activite1',
-        'Denomination',
-        'Localisation1',
-        'numRccm',
-        'Activite2',
-        'numeroDeLaDfe',
-        'Localisation2',
-        'numcnps',
-        'Projet',
-        'CoutestimatifEnlettre',
-        'CoutestimatifEnchiffre',
+        'activite1',
+        'denomination',
+        'localisation1',
+        'num_rccm',
+        'activite2',
+        'numero_de_la_dfe',
+        'localisation2',
+        'num_cnps',
+        'projet',
+        'cout_estimatif_en_lettre',
+        'cout_estimatif_en_chiffre',
     ];
 
-    public function habitation(){
-        return $this->belongsTo(Habitation::class,'id_agent');
+    public function habitation()
+    {
+        return $this->belongsTo(Habitation::class, 'id_habitation');
     }
 }
