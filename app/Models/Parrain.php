84844ebea6b443d736baad84866e1e5b7c78fd42
Axel Toure <axeltoure1eme@gmail.com>
2023-09-19 23:27:59 +0000

    Creation des colonne aux nouvelle règles [a089a790a11feb7d5935aab5f9fb314d37473f24]

--- app/Models/Parrain.php
+++ app/Models/Parrain.php
@@ -6,15 +6,46 @@ use App\Models\Artisan;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Modèle représentant un parrain.
+ *
+ * @property int $id
+ * @property string $nom_parrain
+ * @property string $prenom_parrain
+ * @property string $sexe_parrain
+ * @property string $profession_parrain
+ * @property string $appreciation_parrain
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ *
+ * @property-read \App\Models\Artisan $artisan
+ *
+ * @package App\Models
+ */
 class Parrain extends Model
 {
     use HasFactory;
 
-    protected $fillable=[
-        'Nom',"Prenom","sexe","Profession","Appreciation",
+    /**
+     * Les attributs qui sont mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'nom_parrain',
+        'prenom_parrain',
+        'sexe_parrain',
+        'profession_parrain',
+        'appreciation_parrain',
     ];
 
-    public function artisan(){
-        return $this->belongsTo(Artisan::class,'id_parrain');
+    /**
+     * Obtenez l'artisan associé à ce parrain.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function artisan()
+    {
+        return $this->belongsTo(Artisan::class, 'id_parrain');
     }
 }
