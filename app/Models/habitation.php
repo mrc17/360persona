66166844531fbda31652427b7db2f11bd5b94a67
Axel Toure <axeltoure1eme@gmail.com>
2023-09-20 11:16:02 +0000

    Correction des colonnes

--- app/Models/habitation.php
+++ app/Models/habitation.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Models;
 
 use App\Models\Charge;
@@ -13,37 +14,44 @@ class Habitation extends Model
     use HasFactory;
 
     protected $fillable = [
-        'Ville',
-        'Commune',
-        'Quartier',
-        'SituationMatrimoliale',
-        'RégimeMatrimoliale',
-        'id_Assurance',
+        'ville',
+        'commune',
+        'quartier',
+        'situation_matrimoliale', // Assurez-vous que ce nom correspond à la colonne dans votre base de données.
+        'regime_matrimoliale', // Assurez-vous que ce nom correspond à la colonne dans votre base de données.
+        'id_assurance',
         'id_finance',
         'organisation_id',
         'charge_id',
     ];
 
-    public function artisans(){
+    public function artisans()
+    {
         return $this->hasMany(Artisan::class, 'id_habitation');
     }
 
-    public function assurances(){
-        return $this->belongsTo(Assurance::class, 'id_Assurance');
+    public function assurance()
+    {
+        return $this->belongsTo(Assurance::class, 'id_assurance');
     }
 
-    public function finances(){
-        return $this->belongsTo(Finances::class,'id_finance');
-    }
-    public function organisation(){
-        return $this->belongsTo(Finances::class,'organisation_id');
-    }
-    public function charge(){
-        return $this->belongsTo(Charge::class,'charge_id');
+    public function finances()
+    {
+        return $this->belongsTo(Finances::class, 'id_finance');
     }
 
-    public function activite(){
-        return $this->belongsTo(Activite::class,'charge_id');
+    public function organisation()
+    {
+        return $this->belongsTo(Organisation::class, 'organisation_id');
     }
 
+    public function charge()
+    {
+        return $this->belongsTo(Charge::class, 'charge_id');
+    }
+
+    public function activite()
+    {
+        return $this->belongsTo(Activite::class, 'charge_id');
+    }
 }
