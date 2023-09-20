6406b6d5167671b80a7fad7bd61a52cde1aef677
Axel Toure <axeltoure1eme@gmail.com>
2023-09-20 08:52:59 +0000

    Ajout du champ de zone de l'agent

--- resources/views/auth/signup.blade.php
+++ resources/views/auth/signup.blade.php
@@ -85,7 +85,7 @@
                                         @csrf
                                         <div class="relative mb-4" data-te-input-wrapper-init>
                                             <input type="text" name="Nom" id="Nom" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
-                                            <label for="Nom" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Nom</label>
+                                            <label for="Nom" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Nom & Prenom</label>
                                             @error('Nom')
                                             <div class="error">{{ $message }}</div>
                                             @enderror
@@ -94,6 +94,13 @@
                                             <input type="tel" name="conctat" id="Conctat" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                                             <label for="Conctat" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">Conctat</label>
                                             @error('conctat')
+                                            <div class="error">{{ $message }}</div>
+                                            @enderror
+                                        </div>
+                                        <div class="relative mb-4" data-te-input-wrapper-init>
+                                            <input type="text" name="zone_agent" id="zone_agent" class="block rounded-t-lg px-2.5 pb-2.5 pt-5 w-full text-sm text-gray-900 bg-gray-50 dark:bg-gray-700 border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-green-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                                            <label for="zone_agent" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-4 z-10 origin-[0] left-2.5 peer-focus:text-white peer-focus:dark:text-white-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4">zone de l'agent</label>
+                                            @error('zone_agent')
                                             <div class="error">{{ $message }}</div>
                                             @enderror
                                         </div>
