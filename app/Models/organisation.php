8877fc605315dd56a096ac8e8976b8f59bc75077
Axel Toure <axeltoure1eme@gmail.com>
2023-09-20 11:16:11 +0000

    Correction des colonnes

--- app/Models/organisation.php
+++ app/Models/organisation.php
@@ -6,15 +6,40 @@ use App\Models\Habitation;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Modèle représentant une organisation.
+ *
+ * @property int $id
+ * @property string $etatOrganisation
+ * @property string $NomOrganisation
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ *
+ * @property-read \App\Models\Habitation $habitation
+ *
+ * @package App\Models
+ */
 class Organisation extends Model
 {
     use HasFactory;
 
-    protected $fillable =[
-        'etat',"Nom"
+    /**
+     * Les attributs qui sont mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'etat_organisation',
+        'nom_organisation',
     ];
 
-    public function habitation(){
-        return $this->belongsTo(Habitation::class,'organisation_id');
+    /**
+     * Obtenez l'habitation associée à cette organisation.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function habitation()
+    {
+        return $this->belongsTo(Habitation::class, 'organisation_id');
     }
 }
