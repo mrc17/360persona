e56451f29ceaed13f75ac9e8205a390e781bf8d9
Axel Toure <axeltoure1eme@gmail.com>
2023-09-23 06:33:36 +0000

    Ajout des routes modifier et la page pour la modification

--- routes/web.php
+++ routes/web.php
@@ -24,17 +24,18 @@ Route::get('/logout', [Authentication::class, 'logout'])->name('logout');
 Route::get('/signup', [Authentication::class, 'showInscription'])->name('LoginSingup');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/Dashboard', [DashboardController::class, 'index'])->name('Dashboard');
     Route::get('/Fiche', [DashboardController::class, 'showFiche'])->name('Fiche');
     Route::get('/Liste', [DashboardController::class, 'showListe'])->name('Liste');
-    Route::get('/Messages', [DashboardController::class, 'showMessages'])->name('Messages');
-    Route::get('/Calendrier', [DashboardController::class, 'showCalendrier'])->name('Calendrier');
-    Route::post('/create/artisan', [ArtisanController::class, 'create'])->name('create-artisan');
-    Route::get('/show/artisan/{artisan}', [ArtisanController::class, 'show'])->name('show-artisan');
-    Route::get('/show/artisan/modify/{artisan}', [ArtisanController::class, 'modify'])->name('modify-artisan');
-    Route::get('/show/artisan/delete/{artisan}', [ArtisanController::class, 'delete'])->name('delete-artisan');
-    Route::post('/show/artisan/search', [ArtisanController::class, 'searchartisan'])->name('search-artisan');
-    Route::get('/showSearchAdvanced', [ArtisanController::class, 'showSearchAdvanced'])->name('showSearchAdvanced');
-    Route::post('/show/artisan/SearchAdvanced', [ArtisanController::class, 'searchartisanavanced'])->name('search-artisan-avanced');
     Route::get('/getColonnes/{table}', [GetColonnesController::class, 'getColonnes']);
+    Route::get('/Dashboard', [DashboardController::class, 'index'])->name('Dashboard');
+    Route::get('/Messages', [DashboardController::class, 'showMessages'])->name('Messages');
+    Route::post('/modify/artisan', [ArtisanController::class, 'modify'])->name('modifyartisan');
+    Route::post('/create/artisan', [ArtisanController::class, 'create'])->name('create-artisan');
+    Route::get('/Calendrier', [DashboardController::class, 'showCalendrier'])->name('Calendrier');
+    Route::get('/show/artisan/{artisan}', [ArtisanController::class, 'show'])->name('show-artisan');
+    Route::post('/show/artisan/search', [ArtisanController::class, 'searchartisan'])->name('search-artisan');
+    Route::get('/show/artisan/delete/{artisan}', [ArtisanController::class, 'delete'])->name('delete-artisan');
+    Route::get('/showSearchAdvanced', [ArtisanController::class, 'showSearchAdvanced'])->name('showSearchAdvanced');
+    Route::get('/modify/show/artisan/{artisan}', [ArtisanController::class, 'showmodifyartisan'])->name('show-modify-artisan');
+    Route::post('/show/artisan/SearchAdvanced', [ArtisanController::class, 'searchartisanavanced'])->name('search-artisan-avanced');
 });
