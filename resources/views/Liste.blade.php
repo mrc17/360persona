327448194f4721cedf13cbc90c287081e2e77f37
Axel Toure <axeltoure1eme@gmail.com>
2023-09-24 23:57:11 +0000

    Ajout du message d'action

--- resources/views/Liste.blade.php
+++ resources/views/Liste.blade.php
@@ -83,15 +83,15 @@
         <div class="h-screen md:block w-11/12  mx-auto px-3 rounded-md overflow-x-auto transition-transform duration-300 ease-in-out">
             <div class="w-11/12 sm:px-8 px-4 mx-auto">
                 @if(session('success'))
-                <div class="flex items-center my-5 p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+                <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
                     <svg class="flex-shrink-0 inline w-4 h-4 mr-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                        <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0  0  1  0  2 Z" />
+                      <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                     </svg>
                     <span class="sr-only">Info</span>
                     <div>
-                        <span class="font-medium">Succès alert!</span> {{session('success')}}.
+                      <span class="font-medium">Danger alert!</span> {{session('success')}}.
                     </div>
-                </div>
+                  </div>
                 @endif
                 <div class="sm:flex sm:items-center sm:justify-between">
                     <div>
