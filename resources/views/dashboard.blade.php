a37a751fafba3ffcf8574f18e58b51c5b1ac00e6
Axel Toure <axeltoure1eme@gmail.com>
2023-09-25 08:23:11 +0000

    Ajout de fonctionalité la listes des artisan enregistrer par jours

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -120,10 +120,56 @@
                                             </tr>
                                             @endforeach
                                         </tbody>
-
                                     </table>
                                 </div>
+                                <p class="text-indigo-300 text-sm font-medium uppercase py-5 leading-4">LISTE DES ARTISANS ENREGISTRER AUJOURDHUI.</p>
+                                <div class="relative pb-8 overflow-x-auto">
+                                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                        <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                                            <tr>
+                                                <th scope="col" class="px-6 py-3 rounded-l-lg">
+                                                    Numero
+                                                </th>
+                                                <th scope="col" class="px-6 py-3">
+                                                    Nom
+                                                </th>
+                                                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                                                    Prenom
+                                                </th>
+                                                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                                                    Commune
+                                                </th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @if (count($listedesArtisanenregistreparjour)==0)
+                                            <tr class="bg-white dark:bg-gray-800">
+                                                <td colspan="4" class="px-6 py-4 font-medium text-center text-gray-900 whitespace-nowrap dark:text-white">
+                                                    Aucun artisan enregistré aujourd'hui
+                                                </td>
+                                            </tr>
+                                            @else
+                                            @foreach ($listedesArtisanenregistreparjour as $key => $Artisan )
+                                            <tr class="bg-white dark:bg-gray-800">
+                                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                                    {{$rank = $key + 1}}
+                                                </th>
+                                                <td class="px-6 py-4">
+                                                    {{$Artisan->nom}}
+                                                </td>
+                                                <td class="px-6 py-4">
+                                                    {{$Artisan->prenom}}
+                                                </td>
+                                                <td class="px-6 py-4">
+                                                    {{$Artisan->habitation->commune}}
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                            @endif
 
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                             <div id="stats" class="grid gird-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                                 <div class="bg-black/60 to-white/5 p-6 rounded-lg">
@@ -162,7 +208,6 @@
                                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18L9 11.25l4.306 4.307a11.95 11.95 0 015.814-5.519l2.74-1.22m0 0l-5.94-2.28m5.94 2.28l-2.28 5.941" />
                                                     </svg>
-
                                                 </span>
                                             </p>
                                         </div>
@@ -263,8 +308,8 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z" />
                                             </svg></div>
                                         <div class="p-2">
-                                            <p class="text-xl font-bold">{{ $femmesMatures}}</p>
-                                            <p class="text-gray-500 font-medium">Nombre de </p>
+                                            <p class="text-xl font-bold">{{ $nombredesArtisanenregistreparjour }}</p>
+                                            <p class="text-gray-500 font-medium">Nombre des artisans enregistrés aujourd'hui.</p>
                                             <p class="text-gray-500 text-sm">23 Nov 2022</p>
                                         </div>
                                     </div>
