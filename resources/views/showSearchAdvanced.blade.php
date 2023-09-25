166bce97f5bb2ee744a64df4032928d70a67f24e
Axel Toure <axeltoure1eme@gmail.com>
2023-09-25 15:59:35 +0000

    Ajout du titre de la recherche

--- resources/views/showSearchAdvanced.blade.php
+++ resources/views/showSearchAdvanced.blade.php
@@ -149,6 +149,7 @@
                     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                             <div id="pdf" class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
+                                <p class="text-indigo-300 text-sm font-medium uppercase py-5 leading-4">Liste des artisans sur rechercher en fonctions des  {{ implode(',',$columns) }} .</p>
                                 <table id="Tableau" class="min-w-full divide-y text-center divide-gray-200 dark:divide-gray-700">
                                     <thead class="bg-gray-50 dark:bg-gray-800">
                                         <tr id="ligne">
