dbbd03db285967e697eec665620ff100b2dd0c7f
Axel Toure <axeltoure1eme@gmail.com>
2023-09-26 08:41:00 +0000

    Ajout du titre des fitre effectuer

--- resources/views/showSearchAdvanced.blade.php
+++ resources/views/showSearchAdvanced.blade.php
@@ -149,7 +149,7 @@
                     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                             <div id="pdf" class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                                <p class="text-indigo-300 text-sm font-medium uppercase py-5 leading-4">Liste des artisans sur rechercher en fonctions des  {{ implode(',',$columns) }} .</p>
+                                <p class="text-indigo-300 text-center text-sm font-medium uppercase py-5 leading-4">Liste des artisans rechercher en fonctions des  {{ implode(',',$columns) }} .</p>
                                 <table id="Tableau" class="min-w-full divide-y text-center divide-gray-200 dark:divide-gray-700">
                                     <thead class="bg-gray-50 dark:bg-gray-800">
                                         <tr id="ligne">
@@ -527,7 +527,11 @@
             let colonneId = tag.id;
             let colonne = "colonne" + colonneId.split('_')[1];
             let valeur = tag.value;
-            axios.get('/getColonnes/' + valeur)
+
+            // Obtenez l'URL de base dynamiquement
+            let baseUrl = window.location.origin;
+
+            axios.get(baseUrl + '/getColonnes/' + valeur)
                 .then(function(response) {
                     let options = "";
                     response.data.forEach(function(item) {
@@ -645,6 +649,8 @@
                         label.textContent = artisans[ligne.rowIndex - 1]['agent']['zone_agent'];
                     }else if (checkbox.value === "nom_parrain") {
                         label.textContent = artisans[ligne.rowIndex - 1]['parrain']['nom_parrain'];
+                    }else if (checkbox.value === "activite1") {
+                        label.textContent = artisans[ligne.rowIndex - 1]['habitation'][''];
                     }
                     celluleDonnees.appendChild(label);
                     ligne.appendChild(celluleDonnees);
