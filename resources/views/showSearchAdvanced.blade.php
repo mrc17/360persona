079b0cbe18701b3704a51646e72903ae6321c63c
Axel Toure <axeltoure1eme@gmail.com>
2023-09-26 08:45:07 +0000

    bonne disposition

--- resources/views/showSearchAdvanced.blade.php
+++ resources/views/showSearchAdvanced.blade.php
@@ -149,7 +149,7 @@
                     <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                         <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                             <div id="pdf" class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                                <p class="text-indigo-300 text-center text-sm font-medium uppercase py-5 leading-4">Liste des artisans rechercher en fonctions des  {{ implode(',',$columns) }} .</p>
+                                <p class="text-indigo-300 text-sm font-medium uppercase py-5 leading-4">Liste des artisans sur rechercher en fonctions des  {{ implode(',',$columns) }} .</p>
                                 <table id="Tableau" class="min-w-full divide-y text-center divide-gray-200 dark:divide-gray-700">
                                     <thead class="bg-gray-50 dark:bg-gray-800">
                                         <tr id="ligne">
@@ -531,6 +531,8 @@
             // Obtenez l'URL de base dynamiquement
             let baseUrl = window.location.origin;
 
+            console.log(baseUrl);
+
             axios.get(baseUrl + '/getColonnes/' + valeur)
                 .then(function(response) {
                     let options = "";
@@ -650,7 +652,7 @@
                     }else if (checkbox.value === "nom_parrain") {
                         label.textContent = artisans[ligne.rowIndex - 1]['parrain']['nom_parrain'];
                     }else if (checkbox.value === "activite1") {
-                        label.textContent = artisans[ligne.rowIndex - 1]['habitation'][''];
+                        label.textContent = artisans[ligne.rowIndex - 1]['parrain']['nom_parrain'];
                     }
                     celluleDonnees.appendChild(label);
                     ligne.appendChild(celluleDonnees);
